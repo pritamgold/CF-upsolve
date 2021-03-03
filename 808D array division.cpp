0e9b9ded4781d398355fa094525a4573ba4d7925
Pritam Golder <40055266+pritamgold@users.noreply.github.com>
2021-03-04 03:04:43 +0600

    Add files via upload

---
+++ 808D array division.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n; cin>> n;
+    long long S=0;
+    vector<int> A(n+5,0);
+    for(int i=0; i<n; i++) cin>> A[i], S+=A[i];
+    if(S & 1){
+        cout<< "NO\n";
+        return 0;
+    }
+    map<long long, int> first, second;
+    first[A[0]]++;
+    for(int i=1; i<n; i++) second[A[i]]++;
+
+    long long sdash=0;
+    for(int i=0; i<n; i++){
+        sdash += A[i];
+        if(sdash == S/2){
+            cout<< "YES\n";
+            return 0;
+        }
+        if(sdash < S/2){
+            long long x=S/2-sdash;
+            if(second[x]>0){
+                cout<< "YES\n";
+                return 0;
+            }
+        }
+        else{
+            long long x= sdash-S/2;
+            if(first[x]>0){
+                cout<< "YES\n";
+                return 0;
+            }
+        }
+        first[A[i+1]]++;
+        second[A[i+1]]--;
+    }
+    cout<< "NO" << endl;
+    return 0;
+}
